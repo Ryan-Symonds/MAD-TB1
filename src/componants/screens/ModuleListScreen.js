3f4af403290463ddb6ea0012ba3af85576c2a8b0
Ryan-Symonds <ryansymonds128@gmail.com>
2025-10-17 14:56:23 +0100

    populated view screen (Still Need To transfer functionality to entity's to complete task 6) (#5)

--- src/componants/screens/ModuleListScreen.js
+++ src/componants/screens/ModuleListScreen.js
@@ -5,19 +5,22 @@ import initalmodules from "../../Data/modules";
 import ModuleList from "../entity/modules/ModuleList";
 import RederCount from "../UI/RenderCount";
 
-export const ModuleListScreen = () => {
+export const ModuleListScreen = ({ navigation }) => {
   //Instalisation
   //state
   const [modules, setmodles] = useState(initalmodules);
 
   //handlers
+  const handleSelect = (module) =>
+    navigation.navigate("ModuleViewScreen", { module });
+
   const handleDelete = (module) =>
     setmodles(modules.filter((item) => item.ModuleID !== module.ModuleID));
   //view
   return (
     <Screen>
       <RederCount />
-      <ModuleList modules={modules} onSelect={handleDelete} />
+      <ModuleList modules={modules} onSelect={handleSelect} />
     </Screen>
   );
 };
--- src/componants/screens/ModuleViewScreen.js
+++ src/componants/screens/ModuleViewScreen.js
@@ -1,18 +1,57 @@
 import { StyleSheet, Text, View } from "react-native";
 import Screen from "../layout/Screen";
+import FullWidthImage from "react-native-fullwidth-image";
 
-export const ModuleViewScreen = () => {
+export const ModuleViewScreen = ({ navigate, route }) => {
   //Instalisation
+  const { module } = route.params;
   //state
   //handlers
   //view
   return (
     <Screen>
-      <Text> View </Text>
+      <View style={styles.container}>
+        <FullWidthImage
+          source={{ uri: module.ModuleImage }}
+          style={styles.image}
+        />
+
+        <View style={styles.infoTray}>
+          <Text style={styles.boldText}>
+            {module.ModuleCode} {module.ModuleName}
+          </Text>
+          <Text style={styles.text}>{module.ModuleLevel}</Text>
+          <Text style={styles.text}>
+            {module.ModuleLeaderName}{" "}
+            <Text style={styles.dimText}> (Module Leader)</Text>
+          </Text>
+        </View>
+      </View>
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    gap: 5,
+  },
+  image: {
+    borderRadius: 3,
+  },
+
+  infoTray: {
+    gap: 5,
+  },
+  text: {
+    fontsize: 16,
+  },
+  boldText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  dimText: {
+    color: "grey",
+  },
+});
 
 export default ModuleViewScreen;
